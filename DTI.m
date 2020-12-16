06abb8fe5666c513d9a0136bf60e873ca69f2bee
Ben Jeurissen <ben.jeurissen@gmail.com>
2020-12-16 08:27:28 +0100

    Update DTI.m

--- DTI.m
+++ DTI.m
@@ -53,6 +53,7 @@ classdef DTI < LogLinear
             
             % parse DTI specific options
             p = inputParser;
+            p.KeepUnmatched = true;
             p.addOptional('constr', 1);
             p.addOptional('constr_dirs', 100);
             p.parse(varargin{:});
