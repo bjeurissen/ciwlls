058f697dd2fc7aa793159dad9808c59431ba8e91
Ben Jeurissen <ben.jeurissen@gmail.com>
2020-12-16 08:28:02 +0100

    Update QTI.m

--- QTI.m
+++ QTI.m
@@ -53,6 +53,7 @@ classdef QTI < LogLinear
             
             % parse QTI specifc options
             p = inputParser;
+            p.KeepUnmatched = true;
             p.addOptional('constr', [0 0 1 1 1]);
             p.addOptional('constr_dirs', 100);
             p.parse(varargin{:});
